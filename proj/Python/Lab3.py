d5e47692d40bb1cb89b4f870f119cea23743b8f0
Mihail <32331565+MihailAlexandr@users.noreply.github.com>
2017-10-16 17:12:19 +0300

    Update Lab3.py

--- proj/Python/Lab3.py
+++ proj/Python/Lab3.py
@@ -1,25 +1,24 @@
-import math
 from math import log
 
-formula = 1
+form = 1
 
-while formula >=1 | formula <=3 :
+while form <=3 & form >= 1 :
     print("If you want exit press 0")
-    formula = int (input ("Choose the formula: "))
+    form = int (input ("Choose the formula: "))
 
-    if formula == 1:
-        x = (input("Enter x:"))
+    if form == 1:
+        x = int(input("Enter x:"))
         a = int(input("Enter a:"))
         G = (-7*(4*a**2+15*a*x-4*x**2))/(-45*a**2+26*a*x+7*x**2)
         print ("G={}".format(G))
 
-    elif formula == 2:
+    elif form == 2:
         x = int(input("Enter x:"))
         a = int(input("Enter a:"))
         F =(2**(40*(a**2)-107*a*x+63*(x**2)))
-        print ("F={}".format(F))
+        print("F={}".format(F))
 
-    elif formula == 3:
+    elif form == 3:
         x = int(input("Enter x:"))
         a = int(input("Enter a:"))
         Y = log(-a**2-2*a*x+3*x**2+1)
