280e7f8b9696daa047fb698e2d9e2f46eb9b6d50
Mihail <32331565+MihailAlexandr@users.noreply.github.com>
2017-12-04 13:44:55 +0300

    Update main.c

--- proj/C/lab4/main.c
+++ proj/C/lab4/main.c
@@ -3,16 +3,16 @@
 #include <string.h>
 
 int main ()
- {
-     double a , x , G , F , Y, min, max, min_x , max_x , delitel, up, down;
-     int i, step,n;
-     double mas[1000];
+{
+    double a , x , min, max, min_x , max_x , delitel, up, down;
+    int i, step,n;
+    double mas[1000];
 
-     printf ("Формула #1 G = (-7*(4*pow(a, 2)+15*a*x-4*pow(x, 2)))/(-45*pow(a, 2)+26*a*x+7*pow(x, 2))\n");
-     printf ("Формула #2 F = pow(2, (40 * pow(a, 2) - 107 * a * x + 63 * pow(x, 2)))\n");
-     printf ("Формула #3 Y = log(-pow(a, 2) - 2 * a * x + 3 * pow(x, 2) + 1)\n");
-     printf ("Нажмите 4 для выхода");
-     scanf ("%d" , &n);
+    printf ("Формула #1 G = (-7*(4*pow(a, 2)+15*a*x-4*pow(x, 2)))/(-45*pow(a, 2)+26*a*x+7*pow(x, 2))\n");
+    printf ("Формула #2 F = pow(2, (40 * pow(a, 2) - 107 * a * x + 63 * pow(x, 2)))\n");
+    printf ("Формула #3 Y = log(-pow(a, 2) - 2 * a * x + 3 * pow(x, 2) + 1)\n");
+    printf ("Нажмите 4 для выхода");
+    scanf ("%d" , &n);
     while (n > 0 && n < 4)
     {
         printf ("Введите а:");
@@ -35,7 +35,7 @@ int main ()
                 printf ("вычисление G\n");
                 memset (mas,0,sizeof(mas));
                 delitel = ((- 45 * pow (a , 2)) + (26 * a * x) + (7 * pow (x , 2)));
-                if (delitel == (10e-6))
+                if (fabs(delitel) < 1e-06)
                 {
                     printf ("Ошибка\n");
                     continue;
@@ -45,12 +45,10 @@ int main ()
                     for (x = min_x, i=0 ; x <= max_x; x+= (max_x - min_x) / (step - 1), i++)
                     {
                         printf ("\nx=%.3lf\t" , x);
-                        G = (- 7 * ((4 * pow (a , 2)) + (15 * a * x) - (4 * pow (x , 2)))) / delitel;
-                        mas[i]=G;
-                        printf ("G=%.3lf\n" , G);
+                        mas[i] = (- 7 * ((4 * pow (a , 2)) + (15 * a * x) - (4 * pow (x , 2)))) / delitel;
+                        printf ("G=%.3lf\n" , mas[i]);
                     }
                 }
-
                 break;
 
             case 2:
@@ -58,9 +56,8 @@ int main ()
                 for (x = min_x, i=0 ; x <= max_x ; x+= (max_x - min_x) / (step - 1), i++)
                 {
                     printf ("\nx=%.3lf\t" , x);
-                    F = pow (2 , (40 * pow (a , 2) - 107 * a * x + 63 * pow (x , 2)));
-                    mas[i]=F;
-                    printf ("F=%.3lf\n", F);
+                    mas[i] = pow (2 , (40 * pow (a , 2) - 107 * a * x + 63 * pow (x , 2)));
+                    printf ("F=%.3lf\n", mas[i]);
                 }
                 break;
 
@@ -70,25 +67,23 @@ int main ()
                 for (x = min_x, i=0 ; x <= max_x ; x+= (max_x - min_x) / (step - 1), i++)
                 {
                     printf ("\nx=%.3lf\t" , x);
-                    Y = log (- pow (a , 2) - 2 * a * x + 3 * pow (x , 2) + 1);
-                    mas[i]=Y;
-                    printf ("Y=%.3lf\n" , Y);
+                    mas[i] = log (- pow (a , 2) - 2 * a * x + 3 * pow (x , 2) + 1);
+                    printf ("Y=%.3lf\n" , mas[i]);
                 }
                 break;
 
             default:
                 break;
-
         }
-        for(i = 0, min = mas[0], max = mas[0]; i <=step ; i++)
+        
+        for(i = 0, min = mas[0], max = mas[0]; i < step ; i++)
         {
             if(mas[i] > max)
                 max = mas[i];
-            if(mas[i] < min & mas[i] != 0)
+            if(mas[i] < min)
                 min = mas[i];
         }
         printf ("min=%.3lf\tmax=%.3lf\n\n", min, max);
-
 
         printf ("Формула #1 G = (-7*(4*pow(a, 2)+15*a*x-4*pow(x, 2)))/(-45*pow(a, 2)+26*a*x+7*pow(x, 2))\n");
         printf ("Формула #2 F = pow(2, (40 * pow(a, 2) - 107 * a * x + 63 * pow(x, 2)))\n");
